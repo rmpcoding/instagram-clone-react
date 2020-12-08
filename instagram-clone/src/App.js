87a7134575525f858f7d32fa7f6a324d5a2f658a
Robert Parsons <rmpcoding@gmail.com>
2020-12-08 14:17:50 -0800

    Adds pseudocode for building out necessary instagram components

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+
+// Header
+  // IG Logo
+  // Search Bar
+  // Home Icon
+  // Message Icon
+  // Navigation Icon
+  // Like Icon
+  // Profile Avatar Icon
+// Stories Container
+  // Multiple Users (5 on desktop)
+  // Right Arrow Icon
+    // Left Arrow Icon
+// Profile Section
+  // Profile Avatar
+  // Full Name
+  // Switch Button
+// Posts
+  // Post Header
+    // User Avatar Icon
+    // User Name
+    // Location?
+    // Tripple Dot Options (Modal)
+      // Report Button (red text)
+      // Unfollow Button (red text)
+      // Go To Post Button
+      // Cancel Button
+  // User Picture Upload
+  // Post Footer
+    // Icon Row
+      // Heart Icon
+      // Comment Icon
+      // Message Icon
+      // Save Icon
+    // Amount of Likes
+    // Comment Section
+    // View Comments Button
+    // Display Comments (if any)
+    // Time Since Posted
+    // Comment Row
+      // Add Comment Box
+      // Post Button
+
+// Posts
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
