62e93d5522287809f9df10dcd3ed5aabdac35517
Robert Parsons <rmpcoding@gmail.com>
2020-12-09 10:45:19 -0800

    Adds Pseudocode.js file to organize my pseudocode

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,50 +1,6 @@
 import React from 'react';
 import './App.css';
 
-// Header
-// IG Logo
-// Search Bar
-// Home Icon
-// Message Icon
-// Navigation Icon
-// Like Icon
-// Profile Avatar Icon
-// Stories Container
-// Multiple Users (5 on desktop)
-// Right Arrow Icon
-// Left Arrow Icon
-// Profile Section
-// Profile Avatar
-// Full Name
-// Switch Button
-// Posts
-// Post Header
-// User Avatar Icon
-// User Name
-// Location?
-// Tripple Dot Options (Modal)
-// Report Button (red text)
-// Unfollow Button (red text)
-// Go To Post Button
-// Cancel Button
-// User Picture Upload
-// Post Footer
-// Icon Row
-// Heart Icon
-// Comment Icon
-// Message Icon
-// Save Icon
-// Amount of Likes
-// Comment Section
-// View Comments Button
-// Display Comments (if any)
-// Time Since Posted
-// Comment Row
-// Add Comment Box
-// Post Button
-
-// Posts
-
 function App() {
     return (
         <>
@@ -56,7 +12,6 @@ function App() {
                         className="app__headerImage"
                     />
                 </div>
-                <h1>Hello, World!</h1>
             </div>
         </>
     );
---
+++ instagram-clone/src/Pseudocode.js
@@ -0,0 +1,61 @@
+/* -------------------------------------------------------------------------- */
+/*                                 Header Row                                 */
+/* -------------------------------------------------------------------------- */
+
+// IG Logo
+// Search Bar
+// Home Icon
+// Message Icon
+// Navigation Icon
+// Like Icon
+// Profile Avatar Icon
+
+/* -------------------------------------------------------------------------- */
+/*                              Stories Container                             */
+/* -------------------------------------------------------------------------- */
+
+// Multiple Users (5 on desktop)
+// Right Arrow Icon
+// Left Arrow Icon
+
+/* -------------------------------------------------------------------------- */
+/*                               Profile Section                              */
+/* -------------------------------------------------------------------------- */
+
+// Profile Avatar
+// Full Name
+// Switch Button
+
+/* -------------------------------------------------------------------------- */
+/*                                    Posts                                   */
+/* -------------------------------------------------------------------------- */
+
+// Post Header
+// User Avatar Icon
+// User Name
+// Location?
+
+/* ----------------------- Tripple Dot Options (Modal) ---------------------- */
+
+// Report Button (red text)
+// Unfollow Button (red text)
+// Go To Post Button
+// Cancel Button
+// User Picture Upload
+
+/* ------------------------------- Post Footer ------------------------------ */
+// Heart Icon
+// Comment Icon
+// Message Icon
+// Save Icon
+// Amount of Likes
+
+/* ----------------------------- Comment Section ---------------------------- */
+
+// View Comments Button
+// Display Comments (if any)
+// Time Since Posted
+// Comment Row
+// Add Comment Box
+// Post Button
+/* -------------------------------------------------------------------------- */
