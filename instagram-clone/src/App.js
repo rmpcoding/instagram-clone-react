6da781dab18463b9b2bad2a2293d036399c773e6
Robert Parsons <rmpcoding@gmail.com>
2020-12-09 09:47:37 -0800

    Adds IG logo for app__header

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,58 +1,63 @@
+import React from 'react';
 import './App.css';
 
 // Header
-  // IG Logo
-  // Search Bar
-  // Home Icon
-  // Message Icon
-  // Navigation Icon
-  // Like Icon
-  // Profile Avatar Icon
+// IG Logo
+// Search Bar
+// Home Icon
+// Message Icon
+// Navigation Icon
+// Like Icon
+// Profile Avatar Icon
 // Stories Container
-  // Multiple Users (5 on desktop)
-  // Right Arrow Icon
-    // Left Arrow Icon
+// Multiple Users (5 on desktop)
+// Right Arrow Icon
+// Left Arrow Icon
 // Profile Section
-  // Profile Avatar
-  // Full Name
-  // Switch Button
+// Profile Avatar
+// Full Name
+// Switch Button
 // Posts
-  // Post Header
-    // User Avatar Icon
-    // User Name
-    // Location?
-    // Tripple Dot Options (Modal)
-      // Report Button (red text)
-      // Unfollow Button (red text)
-      // Go To Post Button
-      // Cancel Button
-  // User Picture Upload
-  // Post Footer
-    // Icon Row
-      // Heart Icon
-      // Comment Icon
-      // Message Icon
-      // Save Icon
-    // Amount of Likes
-    // Comment Section
-    // View Comments Button
-    // Display Comments (if any)
-    // Time Since Posted
-    // Comment Row
-      // Add Comment Box
-      // Post Button
+// Post Header
+// User Avatar Icon
+// User Name
+// Location?
+// Tripple Dot Options (Modal)
+// Report Button (red text)
+// Unfollow Button (red text)
+// Go To Post Button
+// Cancel Button
+// User Picture Upload
+// Post Footer
+// Icon Row
+// Heart Icon
+// Comment Icon
+// Message Icon
+// Save Icon
+// Amount of Likes
+// Comment Section
+// View Comments Button
+// Display Comments (if any)
+// Time Since Posted
+// Comment Row
+// Add Comment Box
+// Post Button
 
 // Posts
-
-
-
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <>
+            <div className="app">
+                <h1>Hello, World!</h1>
+                <img
+                    src="https://instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+                    alt="Instagram Logo"
+                    className="app__headerImage"
+                />
+            </div>
+        </>
+    );
 }
 
 export default App;
