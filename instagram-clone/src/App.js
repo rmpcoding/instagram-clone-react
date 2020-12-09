1f624dc584d6a1eff114ebca2d2774d83199c24e
Robert Parsons <rmpcoding@gmail.com>
2020-12-09 10:00:01 -0800

    Adds app container and app header styling

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -49,12 +49,14 @@ function App() {
     return (
         <>
             <div className="app">
+                <div className="app__header">
+                    <img
+                        src="https://instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+                        alt="Instagram Logo"
+                        className="app__headerImage"
+                    />
+                </div>
                 <h1>Hello, World!</h1>
-                <img
-                    src="https://instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
-                    alt="Instagram Logo"
-                    className="app__headerImage"
-                />
             </div>
         </>
     );
