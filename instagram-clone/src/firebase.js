4198d8cda5a7419a007f9b36690baaa7a5669872
Robert Parsons <rmpcoding@gmail.com>
2020-12-10 08:24:38 -0800

    Adds firebase initialization to firebase config

--- instagram-clone/src/firebase.js
+++ instagram-clone/src/firebase.js
@@ -1,11 +1,18 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyCTlEqsWCB1L7soMp5IlillZaSloGijnEY",
-    authDomain: "instagram-clone-95041.firebaseapp.com",
-    databaseURL: "https://instagram-clone-95041-default-rtdb.firebaseio.com",
-    projectId: "instagram-clone-95041",
-    storageBucket: "instagram-clone-95041.appspot.com",
-    messagingSenderId: "63045274814",
-    appId: "1:63045274814:web:ec583a8990bf24429822ef",
-    measurementId: "G-WE7H851SWK"
-  };
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: 'AIzaSyCTlEqsWCB1L7soMp5IlillZaSloGijnEY',
+    authDomain: 'instagram-clone-95041.firebaseapp.com',
+    databaseURL: 'https://instagram-clone-95041-default-rtdb.firebaseio.com',
+    projectId: 'instagram-clone-95041',
+    storageBucket: 'instagram-clone-95041.appspot.com',
+    messagingSenderId: '63045274814',
+    appId: '1:63045274814:web:ec583a8990bf24429822ef',
+    measurementId: 'G-WE7H851SWK',
+});
+
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+const storage = firebaseApp.storage();
+
+export { db, auth, storage };
