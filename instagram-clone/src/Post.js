63d8000f4c5cb0ee92eb4b84009f852620ca14da
Robert Parsons <rmpcoding@gmail.com>
2020-12-09 18:10:26 -0800

    Adds useState to map out Post component

--- instagram-clone/src/Post.js
+++ instagram-clone/src/Post.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Post.css';
 import Avatar from '@material-ui/core/Avatar';
 
-function Post() {
+function Post({ username, caption, imageUrl }) {
     return (
         <div className="post">
             <div className="post__header post__text">
@@ -11,15 +11,17 @@ function Post() {
                     alt="Robert Parsons"
                     src="blank_for_now"
                 ></Avatar>
-                <p><b>theadventureking</b></p>
+                <p>
+                    <b>{username}</b>
+                </p>
             </div>
             <img
                 className="post__image"
                 src="https://scontent-sjc3-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/129812536_837727237048632_8848539024195552609_n.jpg?_nc_ht=scontent-sjc3-1.cdninstagram.com&_nc_cat=1&_nc_ohc=VfIYy3_1HcUAX_T8TxK&tp=1&oh=2f978c9cbe6492a516708f7f73139dc3&oe=5FF9AC18"
-                alt="BLM from Nat Geo"
+                alt="Instagram Post" 
             />
             <h4 className="post__text">
-                <b>theadventureking</b> Comment section
+                <b>{username}</b> {caption}
             </h4>
         </div>
     );
