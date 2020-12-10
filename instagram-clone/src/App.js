e3ba909bbe97387b7a55cb04287f0af809bd25b1
Robert Parsons <rmpcoding@gmail.com>
2020-12-10 08:56:18 -0800

    Adds useEffect and links to firebase db inside app component

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Post from './Post';
+import { db } from './firebase'
 import './App.css';
 
 function App() {
@@ -11,6 +12,15 @@ function App() {
                 'https://scontent-sjc3-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/129812536_837727237048632_8848539024195552609_n.jpg?_nc_ht=scontent-sjc3-1.cdninstagram.com&_nc_cat=1&_nc_ohc=VfIYy3_1HcUAX_T8TxK&tp=1&oh=2f978c9cbe6492a516708f7f73139dc3&oe=5FF9AC18',
         },
     ]);
+
+    useEffect(() => {
+        db.collection('posts').onSnapshot(snapshot => {
+            setPosts(snapshot.docs.map(doc => doc.data()))
+        })
+    }, [])
+
+
+
     return (
         <>
             <div className="app">
